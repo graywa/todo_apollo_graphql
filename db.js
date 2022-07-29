c236f46f13a3145f4655182334a13a474081c680
graywa <74241140+graywa@users.noreply.github.com>
2022-07-29 16:19:37 +0300

    add ui

---
+++ db.js
@@ -0,0 +1,11 @@
+module.exports = {
+  todos: [
+    {id: 1, title: "Learn React", user_id: 1, completed: false},
+    {id: 2, title: "Learn Redux", user_id: 1, completed: false},
+    {id: 3, title: "Learn TypeScript", user_id: 2, completed: false},
+  ],
+  users: [
+    {id: 1, name: "John"},
+    {id: 2, name: "Bill"},
+  ]
+}
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+import { Button, FormControl, Input } from '@chakra-ui/react'
+import { useState } from 'react'
+
+const AddTodo = () => {
+  const [text, setText] = useState('')
+
+  const handleAddTodo = () => {
+    if (text.trim().length) {
+      setText('')
+    }
+  }
+
+  const handleKey = (event) => {
+    if (event.key === 'Enter') {
+      handleAddTodo()
+    }
+  }
+
+  return (
+    <FormControl display={'flex'} mt={6}>
+      <Input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleKey}
+      />
+      <Button onClick={handleAddTodo}>Add todo</Button>
+    </FormControl>
+  )
+}
+
+export default AddTodo
